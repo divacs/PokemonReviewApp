d9227e988100a29476f1e6edaf0d8110bcd1bc18
divacs <sonja.divac@yahoo.com>
2023-12-25 14:15:01 +0100

    model fix

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -1,4 +1,6 @@
-﻿namespace PokemonReviewApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokemonReviewApp.Models
 {
     public class Pokemon
     {
@@ -6,7 +8,7 @@
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
         public ICollection<PokemonCategory> PokemonCategories { get; set; }
-        public ICollection<PokemonOwner> PokemonOwners { get; set; }   
     }
 }
